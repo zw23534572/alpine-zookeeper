b52cbd2ebf83a78179468552509762e0917d8fcd
manson.zhou <manson.zhou@dzong.com>
2018-09-28 14:07:43 +0800

    test

--- zookeeper-3.4.9/bin/start.sh
+++ zookeeper-3.4.9/bin/start.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
-
+echo "step1:${BASH_SOURCE-$0}"
 ZOOBIN="${BASH_SOURCE-$0}"
+echo "step2:$ZOOBIN"
 ZOOBIN="$(dirname "${ZOOBIN}")"
+echo "step3:$ZOOBINDIR"
 ZOOBINDIR="$(cd "${ZOOBIN}"; pwd)"
 
+echo "step4:cp $ZOOBINDIR/../conf/zoo_sample.cfg $ZOOBINDIR/../conf/zoo.cfg"
 cp $ZOOBINDIR/../conf/zoo_sample.cfg $ZOOBINDIR/../conf/zoo.cfg
 
+echo "step5:${HOSTNAME##*-} > $ZOOBINDIR/../data/myid"
 echo ${HOSTNAME##*-} > $ZOOBINDIR/../data/myid
 
+echo "step6:$ZOO_COUNT"
 if [ "$ZOO_COUNT" != "" ];then
 	for id in $(seq 1 $ZOO_COUNT)
 	do
@@ -15,4 +20,5 @@ if [ "$ZOO_COUNT" != "" ];then
 	done
 fi
 
+echo "step7:$ZOOBINDIR/zkServer.sh start-foreground"
 $ZOOBINDIR/zkServer.sh start-foreground
