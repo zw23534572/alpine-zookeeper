812358037e4f038a8596c2103d8e9342cee57b1e
manson.zhou <manson.zhou@dzong.com>
2018-09-28 13:50:07 +0800

    zookeeper

--- zookeeper-3.4.9/bin/start.sh
+++ zookeeper-3.4.9/bin/start.sh
@@ -4,25 +4,6 @@ ZOOBIN="${BASH_SOURCE-$0}"
 ZOOBIN="$(dirname "${ZOOBIN}")"
 ZOOBINDIR="$(cd "${ZOOBIN}"; pwd)"
 
-if [ -z $consul_service_name ];then
-    export consul_service_name='zookeeper'
-fi
-mkdir -p /export/servers/consul/
-echo '{
-    "service": {
-        "name": "'$consul_service_name'",
-        "port": 2181,
-        "checks": [
-            {
-                "script": "if test $(netstat -nltp|grep 2181|wc -l) -eq 0 ; then return -1; else return 0; fi",
-                "interval": "5s",
-				"timeout": "2s"
-            }
-        ]
-    }
-}' > /export/servers/consul/consul.json
-
-
 cp $ZOOBINDIR/../conf/zoo_sample.cfg $ZOOBINDIR/../conf/zoo.cfg
 
 echo ${HOSTNAME##*-} > $ZOOBINDIR/../data/myid
